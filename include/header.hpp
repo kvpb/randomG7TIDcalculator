19c149630debbbcb045428c9c2f5dde0f4d85fc5
Karl <local-part@domain>
2022-12-22 22:05:42 +0100

    (include/)header.hpp created.

---
+++ include/header.hpp
@@ -0,0 +1,40 @@
+/*//	Copyright 2022 Karl Vincent Pierre Bertin
+////
+////	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+////
+////	1.	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+////
+////	2.	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+////
+////	3.	Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+////
+*///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef HEADER_H
+# define HEADER_H
+
+//# include <cwchar> // wchar_t
+# include <ciso646> // xor, bitand
+# include <cstdlib> // NULL, STRTOL, SRAND, RAND
+# include <cstdint> // int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t  https://www.ibm.com/docs/en/zos/2.4.0?topic=files-stdinth-integer-types https://opensource.apple.com/source/Libc/Libc-1158.50.2/include/stdint.h.auto.html
+# include <climits> // USHRT_MAX, UINT_MAX  https://www.ibm.com/docs/en/zos/2.4.0?topic=files-limitsh-standard-values-limits-resources https://opensource.apple.com/source/xnu/xnu-344/EXTERNAL_HEADERS/machine/limits.h.auto.html>
+# include <ctime> // Time
+# include <cmath> // pow, log10, log2, floor
+# include <iostream> // COUT
+
+uint8_t	nbrlen( uint32_t n, int8_t b );
+
+#endif
+
+/*//	header.hpp
+////	Random G7TID Calculator
+////
+////	Karl V. P. B. `kvpb` AKA Karl Thomas George West `ktgw`
+////	+1 (DDD) DDD-DDDD
+////	+33 A BB BB BB BB
+////	local-part@domain
+////	local-part@domain
+////	https://www.linkedin.com/in//
+////	https://twitter.com/ktgwkvpb
+////	https://github.com/kvpb
+*///	https://vm.tiktok.com/ZSwAmcFh/
